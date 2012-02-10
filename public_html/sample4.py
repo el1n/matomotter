9f0846bf31a054469369a64f989b08aa6579ab11
Purintai <printer1942@gmail.com>
2012-02-11 02:00:51 +0900

    twiauth_libtest

---
+++ public_html/sample4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import twiauth
+
+def main():
+        print u"test"
+	print twiauth.geturl()
+
+if __name__ == "__main__":
+	main()
---
+++ public_html/twiauth.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time, random, urllib, cgi, hmac, hashlib
+
+def geturl():
+
+        consumer_key = 'WsgAzyPtjNmiRd3F9cVW9w'
+        consumer_secret = 'G7HH3NYFfWMggzpM1cH0eJtBtI0U4G0QpgWvwvrLpcY'
+
+        # Request Token は GET で取得
+        method = 'GET'
+
+        # Request Token 取得の URL
+        request_token_url = 'http://twitter.com/oauth/request_token'
+
+        # 必須パラメータを準備
+        params = {
+                "oauth_consumer_key": consumer_key, # WEB で登録した oauth consumer key
+                "oauth_signature_method": "HMAC-SHA1", # 暗号のアルゴリズム
+                "oauth_timestamp": str(int(time.time())), # unixtime 
+                "oauth_nonce": str(random.getrandbits(64)), # ランダム文字列
+                "oauth_version": "1.0" # バージョン番号
+                }
+        # パラメータをソートし，URIエンコードした key=value の形にして & で繋げます
+        params_str = '&'.join(['%s=%s' % (urllib.quote(key, ''),urllib.quote(params[key], ''))
+                                   for key in sorted(params)])
+
+        # メソッド，URIエンコードした URL,上で作ったパラメータ文字列を & で繋げます 
+        message = '%s&%s&%s' % (method,urllib.quote(request_token_url,''), urllib.quote(params_str,''))
+
+        # consumer_secret を元にキーを作成します 
+        key = "%s&%s" % (consumer_secret, '')
+
+        # キーを元に message で hmac を作成します
+        signature = hmac.new(key, message, hashlib.sha1)
+
+        # そのダイジェストを base64 でエンコードし，余計な空白を取り除きます
+        digest_base64 = signature.digest().encode("base64").strip()
+
+        # 作成したダイジェストをパラメータに追加します
+        params['oauth_signature'] = digest_base64
+
+        # 作成したパラメータを GET のパラメータとして追加します
+        _url = request_token_url+ '?' + urllib.urlencode(params)
+
+        # その URL にアクセスし, ResponseBody を読み込みます
+        result = urllib.urlopen(_url).read()
+
+        # パースします
+        data = cgi.parse_qs(result)
+
+        # 取得できました!!
+        #print data
+        #{'oauth_token_secret': ['xxx'], 'oauth_token': ['xxxx'], 'oauth_callback_confirmed': ['true']}
+        # oauth_token がリクエストトークン
+
+        return u'http://twitter.com/oauth/authorize?oauth_token='+data['oauth_token'][0]
