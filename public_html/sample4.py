ec6fb83d188c310aa3ed3459ef8a56af7702b1ec
Purintai <printer1942@gmail.com>
2012-02-11 03:53:15 +0900

    o;

--- public_html/sample4.py
+++ public_html/sample4.py
@@ -1,9 +1,36 @@
 #!/usr/bin/python
+import os
+import time
+import Cookie
 import twiauth
+import libGAEsession
+
+CALLBACK_URI = "http://localhost:8080/sample4.py"
 
 def main():
-        print u"test"
-	print twiauth.geturl()
+
+        cookie = Cookie.SimpleCookie(os.environ.get("HTTP_COOKIE",""))
+	session = libGAEsession.session()
+
+	if cookie.has_key("sessionid"):
+		if session.load(cookie["sessionid"].value) == None:
+			session.new()
+        
+	session.set("i",session.get("i",0) + 1)
+
+	t = twiauth.geturl(CALLBACK_URI)
+
+	print "Set-Cookie: sessionid=%s; expires=%s; path=/" % (session.getid(),time.strftime("%a, %d-%b-%Y %H:%M:%S GMT",time.gmtime(time.time() + 86400)))
+	print "Content-Type: text/html"
+	print ""
+	#print "id = %s" % (session.getid())
+	#print session.get("i")
+        print t[0]
+        print t[1]
+        print t[2]
+	session.save()
+	
+	
 
 if __name__ == "__main__":
 	main()
--- public_html/twiauth.py
+++ public_html/twiauth.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import time, random, urllib, cgi, hmac, hashlib
 
-def geturl():
+CONSUMER_KEY = 'WsgAzyPtjNmiRd3F9cVW9w'
+CONSUMER_SECRET = 'G7HH3NYFfWMggzpM1cH0eJtBtI0U4G0QpgWvwvrLpcY'
+request_token_url = 'http://twitter.com/oauth/request_token'
 
-        consumer_key = 'WsgAzyPtjNmiRd3F9cVW9w'
-        consumer_secret = 'G7HH3NYFfWMggzpM1cH0eJtBtI0U4G0QpgWvwvrLpcY'
+def geturl(callback):
 
         # Request Token は GET で取得
         method = 'GET'
 
-        # Request Token 取得の URL
-        request_token_url = 'http://twitter.com/oauth/request_token'
-
         # 必須パラメータを準備
         params = {
-                "oauth_consumer_key": consumer_key, # WEB で登録した oauth consumer key
+                "oauth_consumer_key": CONSUMER_KEY, # WEB で登録した oauth consumer key
                 "oauth_signature_method": "HMAC-SHA1", # 暗号のアルゴリズム
                 "oauth_timestamp": str(int(time.time())), # unixtime 
                 "oauth_nonce": str(random.getrandbits(64)), # ランダム文字列
-                "oauth_version": "1.0" # バージョン番号
+                "oauth_version": "1.0", # バージョン番号
+                "oauth_callback": callback
                 }
         # パラメータをソートし，URIエンコードした key=value の形にして & で繋げます
         params_str = '&'.join(['%s=%s' % (urllib.quote(key, ''),urllib.quote(params[key], ''))
@@ -30,7 +28,7 @@ def geturl():
         message = '%s&%s&%s' % (method,urllib.quote(request_token_url,''), urllib.quote(params_str,''))
 
         # consumer_secret を元にキーを作成します 
-        key = "%s&%s" % (consumer_secret, '')
+        key = "%s&%s" % (CONSUMER_SECRET, '')
 
         # キーを元に message で hmac を作成します
         signature = hmac.new(key, message, hashlib.sha1)
@@ -46,13 +44,13 @@ def geturl():
 
         # その URL にアクセスし, ResponseBody を読み込みます
         result = urllib.urlopen(_url).read()
-
+        
         # パースします
         data = cgi.parse_qs(result)
-
+        
         # 取得できました!!
-        #print data
+        #print data.get("oauth_callback_confirmed"), u'http://twitter.com/oauth/authorize?oauth_token='+data['oauth_token'][0], data.get("oauth_token_secret")
         #{'oauth_token_secret': ['xxx'], 'oauth_token': ['xxxx'], 'oauth_callback_confirmed': ['true']}
         # oauth_token がリクエストトークン
 
-        return u'http://twitter.com/oauth/authorize?oauth_token='+data['oauth_token'][0]
+        return data.get("oauth_callback_confirmed"), u'http://twitter.com/oauth/authorize?oauth_token='+data['oauth_token'][0], data.get("oauth_token_secret")
