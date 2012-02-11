8b97e2a9fc613b84eff245ac1123f0ca82d67193
Purintai <printer1942@gmail.com>
2012-02-11 12:45:23 +0900

    small fix 2

--- public_html/sample5.py
+++ public_html/sample5.py
@@ -13,8 +13,8 @@ def main():
 	cookie = Cookie.SimpleCookie(os.environ.get("HTTP_COOKIE",""))
 	session = libGAEsession.session_memcache()
 	param = cgi.FieldStorage()
-	oauth_token = param.getvalue("oauth_token","error").decode("utf-8")
-	oauth_verifier = param.getvalue("oauth_verifier","error").decode("utf-8")
+	oauth_token = param.getvalue("oauth_token","").decode("utf-8")
+	oauth_verifier = param.getvalue("oauth_verifier","").decode("utf-8")
 
         logon = False
 
@@ -22,8 +22,9 @@ def main():
             if session.load(cookie["sessionid"].value.decode("ascii")) == None:
                 session.new()
 
-	if param.has_key("oauth_token") and param.has_key("oauth_verifier"):
+	if param.has_key("oauth_token") and param.has_key("oauth_verifier") and not session.get("token_secret",None) == None:
             ak = twiauth.getatoken(oauth_token, oauth_verifier, session.get("token_secret"))
+            session.set("username", ak[0])
             session.set("access_key", ak[1])
             logon = True
 
@@ -31,16 +32,16 @@ def main():
 	print "Content-Type: text/html"
 	print ""
             
-        if logon == False:
+        if logon == False and session.get("username",None) == None:
             t = twiauth.geturl(CALLBACK_URI)
             session.set("token_secret", t[2])
             print u'<a href="' + t[1] + u'">Login</a>'
         else:
-            print u"logged in as @" + ak[0] + "<br>"
+            logon = True
+            print u"logged in as @" + session.get("username") + "<br>"
 
 	session.save()
-	
-	
+
 
 if __name__ == "__main__":
 	main()
