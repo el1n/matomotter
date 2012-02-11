322e1bdc480d88b4590965930de7f5a48674e375
Purintai <printer1942@gmail.com>
2012-02-12 01:58:12 +0900

    conf

---
+++ public_html/conf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# coding: UTF-8
+
+dict = {
+'consumer_key' : 'WsgAzyPtjNmiRd3F9cVW9w',
+'consumer_secret' : 'G7HH3NYFfWMggzpM1cH0eJtBtI0U4G0QpgWvwvrLpcY',
+}
+
+stdin = sys.stdin
+stdout = sys.stdout
+reload(sys)
+sys.setdefaultencoding('utf-8')
+sys.stdin = stdin
+sys.stdout = stdout
