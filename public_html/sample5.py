4862d0ac7199c74b7c8a69f9a6cd54ea5a880b69
Purintai <printer1942@gmail.com>
2012-02-11 12:11:37 +0900

    small fix

--- public_html/sample5.py
+++ public_html/sample5.py
@@ -16,14 +16,15 @@ def main():
 	oauth_token = param.getvalue("oauth_token","error").decode("utf-8")
 	oauth_verifier = param.getvalue("oauth_verifier","error").decode("utf-8")
 
+        logon = False
+
 	if cookie.has_key("sessionid"):
             if session.load(cookie["sessionid"].value.decode("ascii")) == None:
                 session.new()
 
-        logon = False
-        
 	if param.has_key("oauth_token") and param.has_key("oauth_verifier"):
             ak = twiauth.getatoken(oauth_token, oauth_verifier, session.get("token_secret"))
+            session.set("access_key", ak[1])
             logon = True
 
         print "Set-Cookie: sessionid=%s; expires=%s; path=/" % (session.getid(),time.strftime("%a, %d-%b-%Y %H:%M:%S GMT",time.gmtime(time.time() + 86400)))
@@ -32,6 +33,7 @@ def main():
             
         if logon == False:
             t = twiauth.geturl(CALLBACK_URI)
+            session.set("token_secret", t[2])
             print u'<a href="' + t[1] + u'">Login</a>'
         else:
             print u"logged in as @" + ak[0] + "<br>"
